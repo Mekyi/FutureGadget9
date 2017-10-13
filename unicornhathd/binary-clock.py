6bf4d3994a1e1acf77193ad142b807d57a91870f
Mekyi <noreply>
2017-10-13 22:39:44 +0300

    progress

--- unicornhathd/binary-clock.py
+++ unicornhathd/binary-clock.py
@@ -13,9 +13,14 @@ except ImportError:
 print('Unicorn HAT HD Binary clock. Press Ctrl + C to exit.')
 
 # Constants
+TIME_COLOR = (0,255,0)
+HOUR_COLOR = (255,0,0)  #(0,0,0)
+MINUTE_COLOR = (0,255,0)  #(0,0,0)
+SECOND_COLOR = (255,0,255)  #(0,0,0)
 BACKGROUND_COLOR = (0,153,255)      # RGB
 BORDER_COLOR = (255,0,0)            # RGB
 BINARY_COLOR = (255,0,0)            # RGB
+
 
 def reset_board():  # Set every pixel to background color
     r, g, b = BACKGROUND_COLOR
@@ -26,21 +31,75 @@ def reset_board():  # Set every pixel to background color
                 unicornhathd.set_pixel(x,y,r,g,b)
             else:
                 unicornhathd.set_pixel(x,y,br,bg,bb)
-                print(x, y)
 
 def get_timeData():
-    dt = datetime.datetime()
-    timeData = {'year': dt.year(), 'month': dt.month(), 'day': dt.day(),
-                'hour': dt.hour(), 'minute': dt.minute(), 'second': dt.second}
+    dt = datetime.datetime.now()
+    timeData = {'year': dt.year, 'month': dt.month, 'day': dt.day,
+                'hour': dt.hour, 'minute': dt.minute, 'second': dt.second}
     return timeData
 
-def convert_to_binary(dictionary):  # Converts dictionary items into binary'
-    for i in range(len(dictionary)):
-        dictionary[i]
+
+def split_numbers(number):
+    numberList = [int(x) for x in str(number)]
+    return numberList
+
+
+def split_values(): #dictionary
+    storedDictionary = get_timeData()
+    for i in storedDictionary:
+        storedDictionary[i] = split_numbers(storedDictionary[i])
+    print(storedDictionary)
+    return storedDictionary
+
+
+def to_binary(number):
+    assert type(number) is int
+    binaryNumber = '{0:b}'.format(number)
+    return binaryNumber
+
+def construct_time_list(dictionary):
+    timeList = []
+    timeList.append(dictionary['hour'])
+    timeList.append(dictionary['minute'])
+    timeList.append(dictionary['second'])
+    return timeList
+
+def time_to_hat(list):
+    print(list)
+    ledColor = ()
+    columnIndex = 2
+    rowIndex = 11
+    for unit in range(len(list)):  # Loop through hours, minutes and seconds
+        if unit == 0:
+            ledColor = HOUR_COLOR
+        elif unit == 1:
+            ledColor = MINUTE_COLOR
+        elif unit == 2:
+            ledColor = SECOND_COLOR
+
+        for binary in range(len(list[unit])):  # Single number in hour/minute/second
+            for binaryDigit in reversed(list[unit][binary]):
+                if binaryDigit == '1':
+                    unicornhathd.set_pixel(columnIndex, rowIndex, ledColor[0], ledColor[1],
+                                           ledColor[2])
+                    rowIndex -= 2
+                else:
+                    rowIndex -= 2
+
+            columnIndex += 2
+            rowIndex = 11
+
+
 
 try:
-    #while True:
+    while True:
         reset_board()
+        timeData = split_values()
+        for item in timeData:  # item in dictionary
+            for i in range(len(timeData[item])):  # list in item
+                timeData[item][i] = to_binary(timeData[item][i])
+        timeList = construct_time_list(timeData)
+        time_to_hat(timeList)
         unicornhathd.show()
 except KeyboardInterrupt:
     unicornhathd.off()
